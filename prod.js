e94ffcaf225f7534fa64d10609f60350a8a893be
Kiarie Ian <113228545+Devkiarie@users.noreply.github.com>
2024-01-01 02:27:42 +0300

    Add files via upload

---
+++ prod.js
@@ -0,0 +1,124 @@
+const products = [
+    {
+        "id": 1,
+        "name": "ELIXIR ",
+        "price": 8500.00,
+        "description": "Authentic KAY ALI ELIXIR",
+        "image": "img/products/007.png",
+    },
+
+    {
+        "id": 2,
+        "name": "MUSK ",
+        "price": 8000.00,
+        "description": "real KAY ALI MUSK",
+        "image": "img/products/vic.png",
+    },
+
+    {
+        "id": 3,
+        "name": "Prada perfume ",
+        "price": 4000.00,
+        "description": "Authentic prada perfume",
+        "image": "img/products/vic pink.png",
+    },
+
+    {
+        "id": 4,
+        "name": "Chanel Cologne ",
+        "price": 5000.00,
+        "description": "Real Parfum, Chanel cologne ",
+        "image": "img/products/ccbrown.png",
+    },
+
+    {
+        "id": 5,
+        "name": "Coco Chanel ",
+        "price": 5000.00,
+        "description": "Original scent Coco Chanel parfum 'mademoiselle'",
+        "image": "img/products/CC pink.png",
+    },
+
+    {
+        "id": 6,
+        "name": "creed ",
+        "price": 5000.00,
+        "description": "Original scent of Creed ",
+        "image": "img/products/creed.png",
+    },
+
+    {
+        "id": 7,
+        "name": "Bleu de chanel ",
+        "price": 5000.00,
+        "description": "Original scent of bleu de Chanel parfum ",
+        "image": "img/products/bleu.png"
+    },
+
+    {
+        "id": 8,
+        "name": "Chanel Black Purse ",
+        "price": 15000.00,
+        "description": "Hand crafted chanel purse ",
+        "image": "img/products/chanel black purse.png",
+    },
+
+    {
+        "id": 9,
+        "name": "Louis Vuitton Birkin ",
+        "price": 25000.00,
+        "description": "Authenticated Louis Vuitton Birkin bag ",
+        "image": "img/products/birkin.png",
+    },
+
+    {
+        "id": 10,
+        "name": "LV Belt ",
+        "price": 10000.00,
+        "description": "Original handcrafted leather LV belt",
+        "image": "img/products/lv belt.png",
+    },
+
+    {
+        "id": 11,
+        "name": "Moschino bag ",
+        "price": 10000.00,
+        "description": "Authentic Moschino bag",
+        "image": "img/products/moschino black.png",
+    },
+
+    {
+        "id": 12,
+        "name": "simple prada totebag ",
+        "price": 3000.00,
+        "description": "simple and stylish totebag", 
+        "image": "img/products/prada tote.png",
+    },
+
+    {
+        "id": 13,
+        "name": "Prada Bag (black) ",
+        "price": 10000.00,
+        "description": "Authentic prada bag",
+        "image": "img/products/prada black.png",
+    },
+
+    {
+        "id": 14,
+        "name": "Prada Bag (pink) ",
+        "price": 10000.00,
+        "description": "Authentic prada bag",
+        "image": "img/products/prada pink.png",
+    },
+
+    {
+        "id": 15,
+        "name": "rick owens ",
+        "price": 15000.00,
+        "description": "Authentic rick owens",
+        "image": "img/products/rick owens.png",
+    },
+
+   
+];
+export default products;
